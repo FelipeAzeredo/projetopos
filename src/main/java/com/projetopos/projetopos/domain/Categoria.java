11288c1afabd0e0f6a01213f72d37d2f63eb8cfb
Felipe Limeira de Azeredo <limeirafelipe@gmail.com>
2020-03-01 23:21:33 -0300

    primeira classe de dominio categoria

---
+++ src/main/java/com/projetopos/projetopos/domain/Categoria.java
@@ -0,0 +1,47 @@
+package com.projetopos.projetopos.domain;
+
+import javax.persistence.Entity;
+import java.util.Objects;
+
+public class Categoria  {
+
+    private Integer id;
+    private String nome;
+
+    public Categoria(){}
+
+    public Categoria(Integer id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Categoria categoria = (Categoria) o;
+        return Objects.equals(id, categoria.id) &&
+                Objects.equals(nome, categoria.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome);
+    }
+}
---
+++ src/main/java/com/projetopos/projetopos/resources/CategoriaResource.java
@@ -0,0 +1,31 @@
+package com.projetopos.projetopos.resources;
+
+import com.projetopos.projetopos.domain.Categoria;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+
+    @GetMapping
+    public List<Categoria>  listar(){
+
+        Categoria cat1 = new Categoria(1, "Informática");
+        Categoria cat2 = new Categoria(2, "Escritório");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+
+        return lista;
+    }
+
+
+}
+
+
