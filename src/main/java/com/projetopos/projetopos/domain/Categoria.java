015e02219f3ebc490fb790f427b10d9ad37acdff
Felipe Limeira de Azeredo <limeirafelipe@gmail.com>
2020-03-01 23:40:38 -0300

    banco de dados H2 e criacao automatica da base de dados

--- src/main/java/com/projetopos/projetopos/domain/Categoria.java
+++ src/main/java/com/projetopos/projetopos/domain/Categoria.java
@@ -1,10 +1,16 @@
 package com.projetopos.projetopos.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Objects;
 
+@Entity
 public class Categoria  {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
 
