8f4b96d86e2863d674e00d843d5ccdd3a8b66ebb
Felipe Limeira de Azeredo <limeirafelipe@gmail.com>
2020-03-01 22:45:35 -0300

    Projeto criado

---
+++ src/main/java/com/projetopos/projetopos/ProjetoposApplication.java
@@ -0,0 +1,13 @@
+package com.projetopos.projetopos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoposApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetopos/projetopos/ProjetoposApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetopos.projetopos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoposApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
