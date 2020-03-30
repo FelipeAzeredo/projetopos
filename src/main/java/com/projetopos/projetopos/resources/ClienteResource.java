68a34b263a96eb52fb191ec202efe787a24895ea
Felipe Limeira de Azeredo <limeirafelipe@gmail.com>
2020-03-30 00:52:11 -0300

    autaliando, buscando e deletando um cliente

--- src/main/java/com/projetopos/projetopos/resources/ClienteResource.java
+++ src/main/java/com/projetopos/projetopos/resources/ClienteResource.java
@@ -1,15 +1,21 @@
 package com.projetopos.projetopos.resources;
 
+
 import com.projetopos.projetopos.domain.Categoria;
 import com.projetopos.projetopos.domain.Cliente;
-import com.projetopos.projetopos.services.CategoriaService;
+import com.projetopos.projetopos.dto.CategoriaDTO;
+import com.projetopos.projetopos.dto.ClienteDTO;
 import com.projetopos.projetopos.services.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value="/clientes")
@@ -20,9 +26,48 @@ public class ClienteResource {
 
     @GetMapping(value="/{id}")
     public ResponseEntity<Cliente> find(@PathVariable Integer id){
-
         Cliente cliente = clienteService.find(id);
         return ResponseEntity.ok().body(cliente);
     }
+
+    @GetMapping
+    public ResponseEntity<List<ClienteDTO>> findAll(){
+        List<Cliente> clientes = clienteService.findAll();
+        List<ClienteDTO> clienteDTO = clientes.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(clienteDTO);
+    }
+
+    @GetMapping(value="/page")
+    public ResponseEntity<Page<Cliente>> findPage(@RequestParam(value="page", defaultValue = "0") Integer page,
+                                                       @RequestParam(value="linesPerPage", defaultValue = "24") Integer linesPerPage,
+                                                       @RequestParam(value="orderBy", defaultValue = "nome") String direction,
+                                                       @RequestParam(value="direction", defaultValue = "ASC") String orderBy){
+        Page<Cliente> clientes = clienteService.findPage(page, linesPerPage, orderBy, direction);
+        Page<ClienteDTO> clientesDTO= clientes.map(obj -> new ClienteDTO(obj));
+        return ResponseEntity.ok().body(clientes);
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> insert(@Valid @RequestBody ClienteDTO objDTO){
+        Cliente obj = clienteService.fromDTO(objDTO);
+        obj = clienteService.create(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping(value="/{id}")
+    public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDTO, @PathVariable Integer id){
+        Cliente obj = clienteService.fromDTO(objDTO);
+        obj.setId(id);
+        obj = clienteService.update(obj);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping(value="/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Integer id){
+        clienteService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
 
 }
--- src/main/java/com/projetopos/projetopos/services/ClienteService.java
+++ src/main/java/com/projetopos/projetopos/services/ClienteService.java
@@ -1,13 +1,19 @@
 package com.projetopos.projetopos.services;
 
-import com.projetopos.projetopos.domain.Categoria;
+
 import com.projetopos.projetopos.domain.Cliente;
-import com.projetopos.projetopos.repositories.CategoriaRepository;
+import com.projetopos.projetopos.dto.ClienteDTO;
 import com.projetopos.projetopos.repositories.ClienteRepository;
+import com.projetopos.projetopos.services.exceptions.DataIntegrityException;
 import com.projetopos.projetopos.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -20,6 +26,48 @@ public class ClienteService {
         Optional<Cliente> obj =  clienteRepository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
     }
+
+    public Cliente create(Cliente obj){
+        obj.setId(null);
+        return clienteRepository.save(obj);
+    }
+
+    public Cliente update(Cliente obj){
+        Cliente newObj = find(obj.getId());
+        updateData(newObj, obj);
+        return clienteRepository.save(obj);
+    }
+
+    private void updateData(Cliente newObj, Cliente obj) {
+        newObj.setNome(obj.getNome());
+        newObj.setEmail(obj.getEmail());
+    }
+
+    public void delete(Integer id){
+        find(id);
+        try{
+            clienteRepository.deleteById(id);
+        }catch(DataIntegrityViolationException e){
+            throw  new DataIntegrityException("Não e possivel excluir categorias que possui produtos");
+        }
+    }
+
+    public List<Cliente> findAll(){
+        return clienteRepository.findAll();
+    }
+
+    public Page<Cliente> findPage(Integer page, Integer linesPerPage, String direction, String orderBy){
+
+        PageRequest pageRequest = PageRequest.of(page,linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        return  clienteRepository.findAll(pageRequest);
+
+    }
+
+    public Cliente fromDTO(ClienteDTO objDTO){
+        return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(), null, null);
+    }
+
+
 
 
 
