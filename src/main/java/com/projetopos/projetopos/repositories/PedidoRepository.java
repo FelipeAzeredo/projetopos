2ce5dd03fb5c1e1935920ce11c75e69a4698d389
Felipe Limeira de Azeredo <limeirafelipe@gmail.com>
2020-05-25 21:39:39 -0300

    configuracao inicial do spring security

--- src/main/java/com/projetopos/projetopos/repositories/PedidoRepository.java
+++ src/main/java/com/projetopos/projetopos/repositories/PedidoRepository.java
@@ -1,9 +1,18 @@
 package com.projetopos.projetopos.repositories;
 
+import com.projetopos.projetopos.domain.Cliente;
 import com.projetopos.projetopos.domain.Pedido;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+
+    @Transactional(readOnly = true)
+    Page<Pedido> findByCliente(Cliente cliente, Pageable pageRequest);
 }
