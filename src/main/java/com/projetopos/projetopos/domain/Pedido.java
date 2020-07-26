9e4eb3e07abe0d971dc506593c49428cb319a764
Felipe Limeira de Azeredo <limeirafelipe@gmail.com>
2020-07-26 17:15:06 -0300

    subtotal e total do pedido

--- src/main/java/com/projetopos/projetopos/domain/Pedido.java
+++ src/main/java/com/projetopos/projetopos/domain/Pedido.java
@@ -38,6 +38,14 @@ public class Pedido {
         this.enderecoDeEntrega = enderecoDeEntrega;
     }
 
+    public double getValorTotal() {
+        double soma = 0.0;
+        for (ItemPedido ip : itens) {
+            soma = soma + ip.getSubTotal();
+        }
+        return soma;
+    }
+
     public Integer getId() {
         return id;
     }
